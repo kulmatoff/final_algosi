fc513738cf42ec675eb46afa1bb5d3ec1cb013c5
kulmatoff <bayastansandman@gmail.com>
2024-06-03 10:19:34 +0600

    Added program.cs file

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http.Headers;
+using System.Numerics;
+using System.Xml.Linq;
+
+
+namespace Final_ALgosi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
